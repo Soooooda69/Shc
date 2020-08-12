a872252ccb92a93583074811c517b728468726cf
Shc <2285124906@qq.com>
2020-08-12 12:05:35 +0800

    finished LinkedListDeque

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,116 +1,147 @@
 public class LinkedListDeque <T> {
 
+    /**Creates an empty linked list deque.*/
+    public LinkedListDeque() {
+        sentinel = new deque(null, null ,null);
+        sentinel.back = sentinel;
+        sentinel.front = sentinel;
+        size =0;
+    }
+
+    public LinkedListDeque(T i) {
+        sentinel = new deque(null, null, null);
+        sentinel.back = new deque(null, i, null);
+        sentinel.front = sentinel.back;
+        size = 1;
+    }
+
     public class deque {
-        public deque front;
         public T item;
+        public deque front;
         public deque back;
 
-
         public deque(deque f, T i, deque b) {
-
             front = f;
             item = i;
             back = b;
         }
     }
-/** the 1st item before adding is add.next*/
-    private deque add;
+
+    private deque sentinel;
     private int size;
 
-    public LinkedListDeque (deque f,T i,deque b){
-        add = new deque (null,null, null);
-        add.front = add;
-        add.back = add;
-        size = 0;
-    }
-
-
+    /**
+     * Adds an item of type T to the front of the deque.
+     */
     public void addFirst(T item) {
-        /*Adds an item of type T to the front of the deque.*/
-        /**add and remove operations must not involve any looping or recursion.
-         *  A single such operation must take “constant time”,
-         *   i.e. execution time should not depend on the size of the deque.
-         */
-        add.back = new deque(add, item, add.back.back);
+        sentinel.back = new deque(sentinel, item, sentinel.back);
+        sentinel.back.back.front = sentinel.back;
         size += 1;
-
-
-
     }
 
-
-
-    public void addLast(T t){
-        /**Adds an item of type T to the back of the deque.*/
-        add.back.item = t;
+    /**
+     * Adds an item of type T to the back of the deque.
+     */
+    public void addLast(T item) {
+        sentinel.front = new deque(sentinel.front, item, sentinel);
+        sentinel.front.front.back = sentinel.front;
         size += 1;
-
     }
 
-
-    public int size(){
-        /**Returns the number of items in the deque.*/
+    /**
+     * Returns the number of items in the deque
+     */
+    public int size() {
         return size;
     }
 
-
-    public boolean isEmpty(int size){
-        /**Returns true if deque is empty, false otherwise.*/
-        if (size == 0 ){
+    /**
+     * Returns true if deque is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        if (size == 0) {
             return true;
         }
         return false;
     }
 
-
-
-    public void printDeque(){
-        /**
-         * Prints the items in the deque from first to last, separated by a space.
-         * Once all the items have been printed, print out a new line.
-         */
-        if (size == 0){
-            System.out.println("Deque is empty.");
-        }
-        System.out.print(add.item);
-    }
-
-
-    public T removeFirst(){
-        /**
-         * Removes and returns the item at the front of the deque. If no such item exists, returns null.
-         */
-        if (add.front.item != null){
+    /**
+     * Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null.
+     */
+    public T removeFirst() {
+        T i = sentinel.back.item;
+        if (isEmpty() == false) {
+            sentinel.back = sentinel.back.back;
+            sentinel.back.front = sentinel;
+            size -= 1;
+            return i;
+        } else {
             return null;
         }
-        add.front.item = null;
-        return null;
-
     }
 
-
-    public T removeLast(){
-        /**
-         * Removes and returns the item at the back of the deque. If no such item exists, returns null.
-         */
-        if (add.back.item != null){
+    /**
+     * Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null.
+     */
+    public T removeLast() {
+        T i = sentinel.front.item;
+        if (isEmpty() == false) {
+            sentinel.front = sentinel.front.front;
+            sentinel.front.back = sentinel;
+            size -= 1;
+            return i;
+        } else  {
             return null;
         }
-        add.back.item = null;
-        return null;
-
     }
 
+    /**Prints the items in the deque from first to last,
+     *separated by a space. Once all the items have been printed,
+     *  print out a new line.*/
+    public void printDeque() {
+        deque print = sentinel.back;
+        for (int i=0; i<size; i+=1) {
+            System.out.print(print.item +" ");
+            print = print.back;
+        }
+        System.out.println();
+    }
 
-    public T get(int index){
-        /**
-         * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
-         * If no such item exists, returns null. Must not alter the deque!
-         */
-        if (size == 0){
+    /**Gets the item at the given index, where 0 is the front,
+     *1 is the next item, and so forth. If no such item exists,
+     * returns null. Must not alter the deque!*/
+    public T get(int index) {
+        deque get = sentinel.back;
+        for (int i=0; i<index; i++) {
+            get = get.back;
+        }
+        return get.item;
+    }
+
+    /**Same as get, but uses recursion.*/
+    private T getR(int i, deque get){
+        if (i == 0){
+            return get.item;
+        }
+        return getR(i-1, get.back);
+    }
+    public T getRecursive(int index) {
+        if (index>size-1){
             return null;
         }
-        return add.item;
+        return getR(index, sentinel.back);
+    }
 
+    /**Creates a deep copy.*/
+    public LinkedListDeque(LinkedListDeque other){
+        sentinel = new deque(null, null, null);
+        sentinel.front = sentinel;
+        sentinel.back = sentinel;
+        size = 0;
+        for (int i=0; i<other.size(); i++){
+            addLast((T) other.get(i));
+        }
     }
 }
